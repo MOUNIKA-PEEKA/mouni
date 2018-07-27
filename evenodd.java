7b921a9e5557efe6c6d916bc3f1c21eba59d8386
MOUNIKA-PEEKA <41738491+MOUNIKA-PEEKA@users.noreply.github.com>
2018-07-27 15:22:37 +0530

    Create evenodd.java

---
+++ evenodd.java
@@ -0,0 +1,26 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+import java.util.Scanner;
+class evenodd
+{
+  public static void main(String args[]) throws java.lang.Exception
+  {
+    Scanner sc=new Scanner(System.in);
+    long n=sc.nextLong();
+    if(n<0)
+    {
+      System.out.println("invalid");
+    }
+    else if(n%2==0)
+    {
+      System.out.println("Even");
+    }
+    else
+    {
+      System.out.println("Odd");
+     }
+   }
+  }
