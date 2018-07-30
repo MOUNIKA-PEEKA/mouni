8d8799b872e130e16e384f4c6d0b728dcd207c9a
MOUNIKA-PEEKA <41738491+MOUNIKA-PEEKA@users.noreply.github.com>
2018-07-30 10:38:01 +0530

    Create Alphabet.java

---
+++ Alphabet.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+import java.util.Scanner;
+class Alphabet
+{
+    public static void main (String[] args) throws java.lang.Exception
+    {
+        char al;
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter a Character : ");
+        al = scan.next().charAt(0);
+        if((al>='a' && al<='z') || (al>='A' && al<='Z'))
+        {
+            System.out.print(al+ " \nAlphabet");
+        }
+        else
+        {
+            System.out.print(al+ "\nNo");
+        }
+    }
+}
+
+
